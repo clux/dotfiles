435ddd85a606a6287f6d545bc9a276041e4b9513
clux <analsandblaster@gmail.com>
2015-11-08 18:47:46 +0000

    symlink autostart and do mkdirps at the top

--- install.sh
+++ install.sh
@@ -3,17 +3,21 @@ set -euo pipefail
 
 cd $(dirname $0)
 
+# Ensure directory trees exist
+mkdir -p ~/.config/sublime-text-3/Packages
+mkdir -p ~/.templates/npm
+
 echo "Symlinking .files to ~/"
 find $PWD -name ".*" -not -name ".gitignore" -type f -print -exec ln -sfn {} ~/ \;
 echo "Symlinking files in .config to ~/.config"
 find $PWD/.config -maxdepth 1 -type f -print -exec ln -sfn {} ~/.config \;
 
 echo "Symlinking files in .templates to ~/.templates"
-mkdir -p ~/.templates/npm
 find $PWD/.templates/npm -maxdepth 1 -type f -print -exec ln -sfn {} ~/.templates/npm \;
 
-# symlink remaining folders that do not match normal patterns
-mkdir -p ~/.config/sublime-text-3/Packages
-echo "ln -sf ~/.config/sublime-text-3/Packages/User/ ~/.config/sublime-text-3/Packages/User"
-# copy static stuff
-cp .config/autostart/* ~/.config/autostart
+# Do the annoying stuff first
+echo "Symlinking User package in sublime-text-3"
+ln -sf ~/.config/sublime-text-3/Packages/User/ ~/.config/sublime-text-3/Packages/User
+
+echo "Symlinking files in .config/autostart to ~/.config/autostart"
+find $PWD/.config/autostart -maxdepth 1 -type f -print -exec ln -sfn {} ~/.config/autostart \;
