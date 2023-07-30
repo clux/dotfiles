750a03062746c1782a47c0bfda796922f4fe886f
clux <sszynrae@gmail.com>
2023-07-30 22:19:10 +0100

    hammerspoon stuff
    Signed-off-by: clux <sszynrae@gmail.com>

--- config/hammerspoon/init.lua
+++ config/hammerspoon/init.lua
@@ -29,18 +29,26 @@ function bindCmd(key, cmd)
   hs.hotkey.bind({"shift", "alt"}, key, function() os.execute(cmd) end)
 end
 
--- window moving between monitors
-bindCmd("1", "/opt/homebrew/bin/yabai -m window --space 1; /opt/homebrew/bin/yabai -m display --focus 1")
-bindCmd("2", "/opt/homebrew/bin/yabai -m window --space 2; /opt/homebrew/bin/yabai -m display --focus 2")
-bindCmd("3", "/opt/homebrew/bin/yabai -m window --space 3; /opt/homebrew/bin/yabai -m display --focus 3")
+-- window moving between monitors/spaces
+-- NB: might rely on displays have separate spaces option
+bindCmd("1", "/opt/homebrew/bin/yabai -m window --space 1; /opt/homebrew/bin/yabai -m space --focus 1")
+bindCmd("2", "/opt/homebrew/bin/yabai -m window --space 2; /opt/homebrew/bin/yabai -m space --focus 2")
+bindCmd("3", "/opt/homebrew/bin/yabai -m window --space 3; /opt/homebrew/bin/yabai -m space --focus 3")
 
 -- window resize
-bindCmd("down", "/opt/homebrew/bin/yabai -m window --resize bottom:0:20") -- increase down
-bindCmd("right",  "/opt/homebrew/bin/yabai -m window --resize right:20:0") -- increase right
+bindCmd("down",  "/opt/homebrew/bin/yabai -m window --resize bottom:0:20") -- increase down
+bindCmd("right", "/opt/homebrew/bin/yabai -m window --resize right:20:0") -- increase right
 bindCmd("up",    "/opt/homebrew/bin/yabai -m window --resize bottom:0:-20") -- decrease down
 bindCmd("left",  "/opt/homebrew/bin/yabai -m window --resize right:-20:0") -- decrease right
 
 -- set a thing to be sticky (excludes it from bsp tiling)
-bindCmd("4", "/opt/homebrew/bin/yabai -m window --toggle sticky; /opt/homebrew/bin/yabai -m window --toggle topmost")
 bindCmd("y", "/opt/homebrew/bin/yabai -m space --layout bsp")
 bindCmd("u", "/opt/homebrew/bin/yabai -m space --layout float")
+bindCmd("p", "/opt/homebrew/bin/yabai -m window --toggle float")
+bindCmd("f", "/opt/homebrew/bin/yabai -m window --toggle native-fullscreen")
+
+-- layout experiments
+--bindCmd("b", "/opt/homebrew/bin/yabai -m window --grid 5:5:1:1:3:3") -- center
+bindCmd("b", "/opt/homebrew/bin/yabai -m space --layout float; /opt/homebrew/bin/yabai -m window --grid 1:1:0:0:1:1; ") -- float and focus one window
+
+-- TODO: global configs to set: mouse_follow_focus, mouse_modifier to super? and mouse_action1 == move, mouse_action2 == resize
